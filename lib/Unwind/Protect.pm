e8a46b95c9f08ba86304ad545e22cbfedc0efd79
Shawn M Moore <sartak@gmail.com>
2009-04-04 16:04:42 -0400

    Doc

--- lib/Unwind/Protect.pm
+++ lib/Unwind/Protect.pm
@@ -45,3 +45,47 @@ sub unwind_protect (&@) {
 
 1;
 
+__END__
+
+=head1 NAME
+
+Unwind::Protect - Run code after other code, even with exceptions
+
+=head1 SYNOPSIS
+
+    ++$depth;
+    
+    unwind_protect { unsafe_function() }
+      after => sub { --$depth };
+
+=head1 DESCRIPTION
+
+Sometimes you need to run code after some other code. Two complications are
+preserving list; scalar; or void context, and dealing with exceptions.
+C<Unwind::Protect> handles both for you.
+
+C<unwind-protect> exists in Common Lisp with nearly the same interface.
+
+I strive to handle exceptions properly. It's not easy, because of all the tools
+Perl provides. C<$SIG{__DIE__}> and C<caller> make this code somewhat hairy. If
+you provide me a failing test where the C<unwind_protect> stack frame is
+inadvertantly exposed, I will try to fix it.
+
+=head1 FUNCTIONS
+
+=head2 unwind_protect CODE, ARGS
+
+C<unwind_protect> takes a code block (similar to C<grep>) then some named
+arguments. Currently the only named argument that is accepted is C<after>
+which is a coderef to run after the primary code.
+
+=head1 AUTHOR
+
+Shawn M Moore, C<sartak@bestpractical.com>
+
+=head1 SEE ALSO
+
+L<Context::Preserve>
+
+=cut
+
